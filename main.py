9e060b9139d5b9dd0053eb7a2bcc1baf13b5ae20
LE FLEM Paul <leflem@et.esiea.fr>
2024-03-11 13:40:51 +0100

    working POC

--- main.py
+++ main.py
@@ -1,83 +1,100 @@
 import random
 import csv
 
-TABLE_SIZE = 10
+TABLE_SIZE = 50
+
+START_POS = 3;
+END_POS = TABLE_SIZE - 3
+
 global cities
 cities = []
 
 def initCities():
     global cities
     cities = []
+    processed = []
     
     for i in range(0, TABLE_SIZE):
         cities.insert(i, [])
         for j in range(0, TABLE_SIZE):
-            if random.randint(0, 50) == 0:
-            # if i == 5 and j == 5:
+            if (random.randint(0, 10) == 0) or (i == START_POS and j == START_POS) or (i == END_POS and j == END_POS):
+            # if (i == 4 and j == 3) or (i == 6 and j == 6) or (i == 9 and j == 3)or (i == 6 and j == 9) or (i == 9 and j == 90 )or (i == 0 and j == 9) or (i == 0 and j == 0):
                 cities[i].insert(j, 'ville')
             else:
                 cities[i].insert(j, 'rien')
-                
-    # for i in range(0, TABLE_SIZE):
-    #     for j in range(0, TABLE_SIZE):
-            
-    #         if cities[i][j] == 'ville':     
-                
-    #             for x in range(i+1, i+4, TABLE_SIZE):
-    #                 for y in range(j+1, j+4, TABLE_SIZE):
-                        
-    #                     if x < len(cities) and y < len(cities[0]):                    
-    #                         if cities[x][y] == 'ville':
-    #                             print('ville base: x ', str(i) + " - y: ", str(j))
-    #                             print('ville voisine: x ', str(x) + " - y: ", str(y) + '\n')
-                            
-    #                         if cities[x][y] == 'ville':
-    #                             for xx in range(i+1, x):
-    #                                 if cities[xx][y] != 'ville':
-    #                                     cities[xx][y] = 'chemin'
-    #                             for yy in range(j+1, y):
-    #                                 if cities[x][yy] != 'ville':
-    #                                     cities[x][yy] = 'chemin' 
+    
+    chemins = []
     
     for i in range(0, TABLE_SIZE):
         for j in range(0, TABLE_SIZE):
             if cities[i][j] == 'ville':
-                for x in range(max([0, i-3]), min([i+4, TABLE_SIZE])):
-                    for y in range(max([0, j-3]), min([j+4, TABLE_SIZE])):
-                        if i != x or j != y:
+                for x in range(max([0, i-5]), min([i+5, TABLE_SIZE])):
+                    for y in range(max([0, j-5]), min([j+5, TABLE_SIZE])):
+                        string = (i*j)+(x*y)
+                        if (i != x or j != y) and string not in processed :
                             if cities[x][y] == 'ville':
-                                print('city 1: ', str(i), str(j)) 
-                                print('city 2: ', str(x), str(y), "\n") 
+                                processed.append(string)
+                                
                                 for xx in range(min([i, x]), max([i, x])):
-                                    if cities[xx][y] != 'ville':
-                                        cities[xx][y] = 'chemin'
-                                for yy in range(min([j, y]), max([j, y])):
+                                    if cities[xx][j] != 'ville':
+                                        distance = (max([i, x]) - min([i, x]) + max([j, y]) - min([j, y])) - 1
+                                        cities[xx][j] = distance
+
+                                        chemins.append({
+                                            'prev': {
+                                                'x': i,
+                                                'y': j
+                                            },
+                                            'next': {
+                                                'x': x,
+                                                'y': y
+                                            },
+                                            'distance': distance
+                                        })
+                                
+                                for yy in range(min([j, y]), max([j+1, y+1])):
                                     if cities[x][yy] != 'ville':
-                                        cities[x][yy] = 'chemin' 
+                                        distance = (max([i, x]) - min([i, x]) + max([j, y]) - min([j, y])) - 1
+                                        cities[x][yy] = distance
+
+                                        chemins.append({
+                                            'prev': {
+                                                'x': i,
+                                                'y': j
+                                            },
+                                              'next': {
+                                                'x': x,
+                                                'y': y
+                                            },
+                                            'distance': distance
+                                        })
                             # else:
                             #     cities[x][y] = 'chemin'
-                
+                                  
 def display():
     global cities
     
     buffer = '  '
     for i in range(0, TABLE_SIZE):
-        buffer = buffer + str(i) + ' '
+        buffer = buffer + str(i).ljust(3, ' ')
         
     print(buffer)
     
     for i in range(0, TABLE_SIZE):
         buffer = ''
         for j in range(0, TABLE_SIZE):
-            if cities[i][j] == 'ville':
-                buffer = buffer + '◼'
-            elif cities[i][j] == 'chemin':
-                buffer = buffer + '⧄'
+            if i == START_POS and j == START_POS:
+                buffer = buffer + str('s').ljust(3, ' ')
+            elif i == END_POS and j == END_POS:
+                buffer = buffer + str('e').ljust(3, ' ')
+            elif cities[i][j] == 'ville':
+                buffer = buffer + str('◼').ljust(3, ' ')
+            elif cities[i][j] == 'rien':
+                buffer = buffer + str('_').ljust(3, ' ')
             else:
-                buffer = buffer + '_'
-            buffer = buffer + ' '
+                buffer = buffer + str(cities[i][j]).ljust(3, ' ')
         
-        print(str(i) + ' ' + buffer)
+        print(str(i).ljust(3, ' ') + buffer)
     print('\n◼ ville')
     print('_ vide')
 
